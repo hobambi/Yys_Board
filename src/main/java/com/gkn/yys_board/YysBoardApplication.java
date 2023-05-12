aafd98a8363f6eae862d470910cc6b889f53a2bf
hobambi <yyunsunny@gmail.com>
2023-05-12 17:01:55 +0900

    Initial Commit

---
+++ src/main/java/com/gkn/yys_board/YysBoardApplication.java
@@ -0,0 +1,13 @@
+package com.gkn.yys_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YysBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YysBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gkn/yys_board/YysBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkn.yys_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YysBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
